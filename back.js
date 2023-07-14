5a4ae2fe099727bef4986e318c9ba3d863cc6b5b
Valeria Cabiya <vcabiya@gmail.com>
2023-07-13 20:33:33 -0400

    flailing about wildly

--- back.js
+++ back.js
@@ -1,6 +1,11 @@
+
 function putBox(){
   var contents = document.saidForm.daQuote.value;
+  contents = contents.substring(0,120);
   document.getElementById('textBox').innerHTML = contents;
+}
+function sendRight(){
+  
 }
 function onFileSelected(event) {
   var selectedFile = event.target.files[0];
@@ -15,4 +20,8 @@ function onFileSelected(event) {
 
   reader.readAsDataURL(selectedFile);
 }
+
+html2canvas(document.querySelector("#quoteBox")).then(canvas => {
+  document.body.appendChild(canvas)
+});
 
